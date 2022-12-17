725949c1be745be9408165b15c3e85f192ff43be
Ali <alipro.ig7@gmail.com>
2022-12-17 16:01:51 +0300

    some update

--- src/App.js
+++ src/App.js
@@ -24,14 +24,29 @@ function App() {
     }
   };
 
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id))
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Header countCart={cartItems.length}/>
-        <Rot products={products} onAdd={onAdd}/>
+        <Header countCart={cartItems.length} />
+        <Rot products={products} onAdd={onAdd} />
         <Routes>
-          
-          <Route path="/cart" element={<Cart cartItems={cartItems} onAdd={onAdd} />} />
+          <Route
+            path="/cart"
+            element={<Cart cartItems={cartItems} onRemove={onRemove} onAdd={onAdd} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 function Cart(props) {
-  const { cartItems, onAdd } = props;
+  const { cartItems, onAdd, onRemove } = props;
 
   const itemPrice = cartItems.reduce(
     (acc, curr) => acc + curr.price * curr.qty,
     0
   );
   const taxPrice = itemPrice * 0.15;
-  const shippingPrice = itemPrice > 65 ? 0 : 20;
+  const shippingPrice = itemPrice > 40 ? 0 : 20;
   const totalPrice = itemPrice + shippingPrice + taxPrice;
 
   return (
@@ -23,7 +23,7 @@ function Cart(props) {
               +
             </button>
             <p>{item.qty}</p>
-            <button className="btn" onClick={() => onAdd(item)}>
+            <button className="btn" onClick={() => onRemove(item)}>
               -
             </button>
             {item.price.toFixed(2)}
